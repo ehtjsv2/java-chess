d059edc52a22a249c1b25293a211e7b5197a365a
ehtjsv2 <ehtjsv2@naver.com>
2024-04-01 17:57:26 +0900

    refactor: DbConnect 클래스 분리

--- src/main/java/chess/dao/SpacesDaoImpl.java
+++ src/main/java/chess/dao/SpacesDaoImpl.java
@@ -1,8 +1,9 @@
 package chess.dao;
 
+import static chess.dao.DbConnector.getConnection;
+
 import chess.domain.chessBoard.Space;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,21 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SpacesDaoImpl implements SpacesDao {
-    private static final String SERVER = "localhost:13306"; // MySQL 서버 주소
-    private static final String DATABASE = "chess"; // MySQL DATABASE 이름
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디\
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
 
-    public Connection getConnection() {
-        try {
-            return DriverManager.getConnection("jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION, USERNAME, PASSWORD);
-        } catch (final SQLException e) {
-            System.err.println("DB 연결 오류:" + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
-    }
 
     @Override
     public int countAll() {
